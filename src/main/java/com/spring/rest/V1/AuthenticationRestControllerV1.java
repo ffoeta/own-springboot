5d012daf6c5375ae9bb577c1a1a1b92277fd22ea
Lesha <lesha@Leshas-MacBook-Pro.local>
2019-10-24 09:11:51 +0300

    signup attempt

--- src/main/java/com/spring/rest/V1/AuthenticationRestControllerV1.java
+++ src/main/java/com/spring/rest/V1/AuthenticationRestControllerV1.java
@@ -37,19 +37,30 @@ public class AuthenticationRestControllerV1 {
 
     @PostMapping("login")
     public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto) {
+        System.out.println("LOGIN");
         String username = requestDto.getUsername();
+        System.out.println("1");
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, requestDto.getPassword()));
+        System.out.println("2");
         User user = userService.findByUsername(username);
+        System.out.println("3");
 
         if (user == null) {
             throw new UsernameNotFoundException("User with username: " + username + " not found");
         }
+        System.out.println("4");
 
         String token = jwtTokenProvider.createToken(username, user.getRoles());
 
+        System.out.println("5");
+
         Map<Object, Object> response = new HashMap<>();
+
+        System.out.println("6");
         response.put("username", username);
         response.put("token", token);
+
+        System.out.println("7");
 
         return ResponseEntity.ok(response);
     }
@@ -61,7 +72,11 @@ public class AuthenticationRestControllerV1 {
         user.setUsername(requestDto.getUsername());
         user.setPassword(requestDto.getPassword());
 
-        userService.register(user,"ROLE_USER");
+        if (user.getUsername().startsWith("admin")) {
+            userService.register(user,"ADMIN_USER");
+        } else {
+            userService.register(user,"ROLE_USER");
+        }
 
         return login(requestDto);
     }
